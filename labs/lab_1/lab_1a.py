52d1ec8e981801065ee3979092fba440915ca534
rockerbird7-max <rockerbird7@gmail.com>
2026-03-28 22:32:01 -0500

    Added Planet to message
    Added fun little extra

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -6,7 +6,7 @@ with your name. Then, save the code, add it to the staging area, and commit it t
 """
 
 def main():
-    print("Hello World!")
+    print("Hello World and Planet!")
 
     name = "Saketh Tammisetti" # TODO: Insert your name between the double quotes
 
